e9971714505e081edd91051ae9150b731fd318a4
Josephglz <joseph220199@gmail.com>
2025-01-21 16:10:25 -0600

    feat: Implement registration form with validation and error handling in register component

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,11 +1,91 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { APIResponse, AuthService } from '../../../services/auth.service';
+import { User } from '../../../models/User';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './register.component.html'
 })
 export class RegisterComponent {
+  _registerForm: FormGroup = {} as FormGroup;
+  loading: boolean = false;
+  errorMessage: string | null = null;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _authService: AuthService,
+  ) {
+    this._registerForm = this._formBuilder.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, this._authService.emailValidator()]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  getUserFromForm(): Partial<User> {
+    return {
+      username: this._registerForm.get('username')?.value,
+      email: this._registerForm.get('email')?.value,
+      password: this._registerForm.get('password')?.value,
+    };
+  }
+
+  hasError(controlName: string, error: string): boolean | undefined {
+    const control = this._registerForm.get(controlName);
+    return control?.hasError(error) && (control?.dirty || control?.touched);
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this._registerForm.get(controlName);
+    if (control?.hasError('required')) {
+      return 'Este campo es obligatorio';
+    }
+    if (control?.hasError('email')) {
+      return 'El correo electrónico introducido es inválido';
+    }
+    if (control?.hasError('minlength')) {
+      return `La contraseña debe tener al menos ${control.errors?.['minlength'].requiredLength} caracteres`;
+    }
+    return '';
+  }
+
+  async onSubmit() {
+    if (this._registerForm.invalid) {
+      this._registerForm.markAllAsTouched();
+      this.errorMessage = 'Por favor, rellena el formulario correctamente';
+      return;
+    }
+
+    const user = this._registerForm.value;
+    this.loading = true;
+    this.errorMessage = null;
+
+    this._authService.register(user).subscribe({
+      next: (response: APIResponse) => {
+        const { token } = response;
+        if(!token || token.length === 0) return;
+        this._authService.saveToken(token);
+        this._router.navigate(['/']);
+      },
+      error: (error) => {
+        this.errorMessage = error.error.message;
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    })
+
+  }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment.development';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,11 @@ export class AuthService {
 
   login(email: string, password: string) {
     const body = { email, password };
-    return this._http.post<LoginResponse>(`${environment.API_URL}/api/auth/login`, body)
+    return this._http.post<APIResponse>(`${environment.API_URL}/api/auth/login`, body)
+  }
+
+  register(user: User) {
+    return this._http.post<APIResponse>(`${environment.API_URL}/api/auth/register`, user);
   }
 
   saveToken(token: string) {
@@ -22,9 +27,20 @@ export class AuthService {
   getToken() {
     return localStorage.getItem('token');
   }
+
+  emailValidator() {
+    const emailRegex =
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return (control: any) => {
+      if (!control.value) return null;
+      return emailRegex.test(control.value) ? null : { email: true };
+    };
+  }
+
 }
 
-export interface LoginResponse {
+export interface APIResponse {
   message: string;
   token: string;
 }
+
