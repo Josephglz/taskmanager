f485169dc181cb3f7520f338d877a6385b426c23
Josephglz <joseph220199@gmail.com>
2025-01-20 19:35:07 -0600

    feat: Add routing for authentication and task list components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { authGuard } from './core/guards/auth.guard';
+import { LoginComponent } from './pages/auth/login/login.component';
+import { RegisterComponent } from './pages/auth/register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Lista de Tareas',
+    component: AppComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'login',
+    title: 'Iniciar Sesión',
+    component: LoginComponent
+  },
+  {
+    path: 'register',
+    title: 'Registrarse',
+    component: RegisterComponent
+  }
+];
