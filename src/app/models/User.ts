0e7803dceceba588070dec2dc6d84de14d9de281
Josephglz <joseph220199@gmail.com>
2025-01-21 14:20:14 -0600

    feat: Make username and created_at properties optional in User interface

--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,8 +1,8 @@
 export interface User {
   id: number;
-  username: string;
+  username?: string;
   email: string;
   password: string;
-  created_at: Date;
+  created_at?: Date;
   token?: string;
 }
