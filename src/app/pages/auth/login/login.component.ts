220d7eb2bb41366ff803d61880003d8c5f45d5fa
Josephglz <joseph220199@gmail.com>
2025-01-21 15:21:45 -0600

    feat: Enhance login component with reactive form, validation, and error handling

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -1,11 +1,97 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { User } from '../../../models/User';
+import { AuthService, LoginResponse } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterModule,
+    ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './login.component.html'
 })
 export class LoginComponent {
+  _loginForm: FormGroup = {} as FormGroup;
+  loading: boolean = false;
+  errorMessage: string | null = null;
+
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _router: Router,
+    private _authService: AuthService,
+  ){
+    this._loginForm = this._formBuilder.group({
+      email: ['', [Validators.required, this.emailValidator()]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    })
+  }
+
+  getUserFromForm(): Partial<User> {
+    return {
+      email: this._loginForm.get('email')?.value,
+      password: this._loginForm.get('password')?.value,
+    };
+  }
+
+  hasError(controlName: string, error: string): boolean | undefined {
+    const control = this._loginForm.get(controlName);
+    return control?.hasError(error) && (control?.dirty || control?.touched);
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this._loginForm.get(controlName);
+    if (control?.hasError('required')) {
+      return 'Este campo es obligatorio';
+    }
+    if (control?.hasError('email')) {
+      return 'El correo electrónico introducido es inválido';
+    }
+    if (control?.hasError('minlength')) {
+      return `La contraseña debe tener al menos ${control.errors?.['minlength'].requiredLength} caracteres`;
+    }
+    return '';
+  }
+
+  async onSubmit() {
+    if (this._loginForm.invalid) {
+      this._loginForm.markAllAsTouched();
+      this.errorMessage = 'Datos de acceso inválidos, por favor rellene los campos correctamente';
+      return
+    }
+
+    const { email, password } = this._loginForm.value
+    this.loading = true;
+    this.errorMessage = null;
+
+    this._authService.login(email, password).subscribe({
+      next: (response: LoginResponse) => {
+        const { token } = response;
+        if(!token || token.length === 0) return;
+        this._authService.saveToken(token);
+        this._router.navigate(['/']);
+      },
+      error: (error) => {
+        this.errorMessage = error.error.message;
+        this.loading = false;
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    })
+  }
+
+  emailValidator() {
+    const emailRegex =
+      /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return (control: any) => {
+      if (!control.value) return null;
+      return emailRegex.test(control.value) ? null : { email: true };
+    };
+  }
 
 }
