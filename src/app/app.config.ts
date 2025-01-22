50d7e56b2a5c7e6d47a02b8dcde6178842afdb0d
Josephglz <joseph220199@gmail.com>
2025-01-21 19:05:13 -0600

    feat: Integrate ngx-toastr for notifications and add animations support

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,6 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { tokenSecurityInterceptor } from './token-security.interceptor';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,6 +15,8 @@ export const appConfig: ApplicationConfig = {
       withInterceptors([
         tokenSecurityInterceptor
       ])
-    )
+    ),
+    provideAnimations(),
+    provideToastr()
   ]
 };
